aeebcbf0bc327d726c13ba4907ea9f47468c23fa
Samuel Liu <samuel.liu1004@gmail.com>
2023-02-27 15:10:21 -0800

    Organize code

--- javascript/blog.js
+++ javascript/blog.js
@@ -4,20 +4,6 @@ let blogPosts = [];
 // blog post add/edit dialog
 const blogDialog = document.getElementById("blog-dialog");
 
-// sample data
-blogPosts.push({
-  title: "About Javascript",
-  date: new Date().toLocaleDateString("en-US"),
-  summary:
-    "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries.",
-});
-blogPosts.push({
-  title: "About Kotlin",
-  date: new Date().toLocaleDateString("en-US"),
-  summary:
-    "Kotlin is a cross-platform, statically typed, general-purpose high-level programming language with type inference. Kotlin is designed to interoperate fully with Java, and the JVM version of Kotlin's standard library depends on the Java Class Library, but type inference allows its syntax to be more concise.",
-});
-
 // function that shows the current blogPosts onto the screen
 function listBlogPosts() {
   const blogList = document.getElementById("blog-list");
@@ -65,31 +51,21 @@ function clearDialogInputs() {
 
 // show dialog function
 function showDialog(method, blogIndex = -1) {
-  if (method == "add") {
-    blogDialog.innerHTML = `<form method="dialog">
-        <legend>Create New Blog Post</legend>
+  let dialogLegend =
+    method == "edit" ? "Edit Blog Post" : "Create New Blog Post";
+  let blogTitle = method == "edit" ? blogPosts[blogIndex].title : "";
+  let blogSummary = method == "edit" ? blogPosts[blogIndex].summary : "";
+
+  blogDialog.innerHTML = `<form method="dialog">
+        <legend>${dialogLegend}</legend>
         <label for="blog-title-input">Title</label><br>
-        <input type="text" id="blog-title-input"></input><br>
+        <input type="text" id="blog-title-input" value="${blogTitle}"></input><br>
 
         <label for="blog-summary-input">Summary</label><br>
-        <textarea id="blog-summary-input" rows="5" cols="70"></textarea><br>
+        <textarea id="blog-summary-input" rows="5" cols="70">${blogSummary}</textarea><br>
 
         <button id="submit-blog-post">Submit</button>
     </form>`;
-  } else if (method == "edit") {
-    const blogPost = blogPosts[blogIndex];
-
-    blogDialog.innerHTML = `<form method="dialog">
-        <legend>Edit Blog Post</legend>
-        <label for="blog-title-input">Title</label><br>
-        <input type="text" id="blog-title-input" value="${blogPost.title}"></input><br>
-
-        <label for="blog-summary-input">Summary</label><br>
-        <textarea id="blog-summary-input" rows="5" cols="70">${blogPost.summary}</textarea><br>
-
-        <button id="submit-blog-post">Submit</button>
-    </form>`;
-  }
 
   // add event listener for submit button
   const submitBlogPostBtn = document.getElementById("submit-blog-post");
@@ -132,6 +108,18 @@ function onSubmitBlogPost(method, blogIndex) {
 }
 
 // initial display of all blog posts
+blogPosts.push({
+  title: "Javascript",
+  date: new Date().toLocaleDateString("en-US"),
+  summary:
+    "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries.",
+});
+blogPosts.push({
+  title: "Kotlin",
+  date: new Date().toLocaleDateString("en-US"),
+  summary:
+    "Kotlin is a cross-platform, statically typed, general-purpose high-level programming language with type inference. Kotlin is designed to interoperate fully with Java, and the JVM version of Kotlin's standard library depends on the Java Class Library, but type inference allows its syntax to be more concise.",
+});
 listBlogPosts();
 
 /*
