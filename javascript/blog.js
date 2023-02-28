5733a1562bc76aee4eabbb1475e53f14c3207a2e
Samuel Liu <samuel.liu1004@gmail.com>
2023-02-28 12:17:31 -0800

    Finish styled blog

--- javascript/blog.js
+++ javascript/blog.js
@@ -1,8 +1,7 @@
+import { clearDialogInputs, showDialog } from "./blog-dialog-utilities.js";
+
 // variable that stores all blog posts in Object format
 let blogPosts = JSON.parse(localStorage.getItem("blog-list")) || [];
-
-// blog post add/edit dialog
-const blogDialog = document.getElementById("blog-dialog");
 
 // function that shows the current blogPosts onto the screen
 function listBlogPosts() {
@@ -26,7 +25,7 @@ function attachBlogPostListeners(blogIndex) {
   const editButton = document.getElementById("edit" + blogIndex);
   const deleteButton = document.getElementById("delete" + blogIndex);
   editButton.addEventListener("click", () => {
-    showDialog("edit", blogIndex);
+    showDialog(blogPosts, "edit", blogIndex);
   });
   deleteButton.addEventListener("click", () => {
     deleteBlogPost(blogIndex);
@@ -41,81 +40,13 @@ function deleteBlogPost(blogIndex) {
   listBlogPosts();
 }
 
-// clear all dialog input fields
-function clearDialogInputs() {
-  const blogTitle = document.getElementById("blog-title-input");
-  const blogSummary = document.getElementById("blog-summary-input");
-  if (blogTitle != null) {
-    blogTitle.value = "";
-  }
-  if (blogSummary != null) {
-    blogSummary.value = "";
-  }
-}
-
-// show dialog function
-function showDialog(method, blogIndex = -1) {
-  let dialogLegend =
-    method == "edit" ? "Edit Blog Post" : "Create New Blog Post";
-  let blogTitle = method == "edit" ? blogPosts[blogIndex].title : "";
-  let blogSummary = method == "edit" ? blogPosts[blogIndex].summary : "";
-
-  blogDialog.innerHTML = `<form method="dialog">
-        <legend>${dialogLegend}</legend>
-        <label for="blog-title-input">Title</label><br>
-        <input type="text" id="blog-title-input" value="${blogTitle}" required></input><br>
-
-        <label for="blog-summary-input">Summary</label><br>
-        <textarea id="blog-summary-input" rows="5" cols="70" required>${blogSummary}</textarea><br>
-
-        <button id="submit-blog-post">Submit</button>
-    </form>`;
-
-  // add event listener for submit button
-  const submitBlogPostBtn = document.getElementById("submit-blog-post");
-  submitBlogPostBtn.addEventListener("click", () => {
-    onSubmitBlogPost(method, blogIndex);
-  });
-
-  // show the dialog
-  blogDialog.show();
-}
 // create event listener for add blog post button
 function addBlogPost() {
   clearDialogInputs();
-  showDialog("add");
+  showDialog(blogPosts, "add");
 }
 const addBlogPostBtn = document.getElementById("add-blog-post");
 addBlogPostBtn.addEventListener("click", addBlogPost);
-
-// create event listener for submit blog post button
-function onSubmitBlogPost(method, blogIndex) {
-  const newBlogTitle = document.getElementById("blog-title-input").value;
-  const newBlogSummary = document.getElementById("blog-summary-input").value;
-
-  // do not add new blog post if user left title or summary field blank
-  if (newBlogTitle == "" || newBlogSummary == "") return;
-
-  if (method == "add") {
-    blogPosts.push({
-      title: newBlogTitle,
-      date: new Date().toLocaleDateString("en-US"),
-      summary: newBlogSummary,
-    });
-  } else if (method == "edit") {
-    blogPosts[blogIndex] = {
-      title: newBlogTitle,
-      date: new Date().toLocaleDateString("en-US"),
-      summary: newBlogSummary,
-    };
-  }
-
-  // convert blogPosts to a string with JSON.stringify and save to localStorage
-  localStorage.setItem("blog-list", JSON.stringify(blogPosts));
-
-  // refresh list to include new blog post
-  listBlogPosts();
-}
 
 // initial display of all blog posts
 if (blogPosts.length == 0) {
@@ -140,7 +71,4 @@ if (blogPosts.length == 0) {
 }
 listBlogPosts();
 
-/*
-TODOS:
-- Put dialog stuff in separate module
-*/
+export { listBlogPosts };
---
+++ javascript/styled-blog-dialog-utilities.js
@@ -0,0 +1,75 @@
+import { listBlogPosts } from "./styledblog.js";
+
+// blog post add/edit dialog
+const blogDialog = document.getElementById("blog-dialog");
+
+// clear all dialog input fields
+function clearDialogInputs() {
+  const blogTitle = document.getElementById("blog-title-input");
+  const blogSummary = document.getElementById("blog-summary-input");
+  if (blogTitle != null) {
+    blogTitle.value = "";
+  }
+  if (blogSummary != null) {
+    blogSummary.value = "";
+  }
+}
+
+// show dialog function
+function showDialog(blogPosts, method, blogIndex = -1) {
+  let dialogLegend =
+    method == "edit" ? "Edit Blog Post" : "Create New Blog Post";
+  let blogTitle = method == "edit" ? blogPosts[blogIndex].title : "";
+  let blogSummary = method == "edit" ? blogPosts[blogIndex].summary : "";
+
+  blogDialog.innerHTML = `<form method="dialog">
+          <legend>${dialogLegend}</legend>
+          <label for="blog-title-input">Title</label><br>
+          <input type="text" id="blog-title-input" value="${blogTitle}" required></input><br>
+  
+          <label for="blog-summary-input">Summary</label><br>
+          <textarea id="blog-summary-input" rows="5" cols="50" required>${blogSummary}</textarea><br>
+  
+          <button id="submit-blog-post">Submit</button>
+      </form>`;
+
+  // add event listener for submit button
+  const submitBlogPostBtn = document.getElementById("submit-blog-post");
+  submitBlogPostBtn.addEventListener("click", () => {
+    onSubmitBlogPost(blogPosts, method, blogIndex);
+  });
+
+  // show the dialog
+  blogDialog.show();
+}
+
+// create event listener for submit blog post button
+function onSubmitBlogPost(blogPosts, method, blogIndex) {
+  const newBlogTitle = document.getElementById("blog-title-input").value;
+  const newBlogSummary = document.getElementById("blog-summary-input").value;
+
+  // do not add new blog post if user left title or summary field blank
+  if (newBlogTitle == "" || newBlogSummary == "") return;
+
+  if (method == "add") {
+    blogPosts.push({
+      title: newBlogTitle,
+      date: new Date().toLocaleDateString("en-US"),
+      summary: newBlogSummary,
+    });
+  } else if (method == "edit") {
+    blogPosts[blogIndex] = {
+      title: newBlogTitle,
+      date: new Date().toLocaleDateString("en-US"),
+      summary: newBlogSummary,
+    };
+  }
+
+  // convert blogPosts to a string with JSON.stringify and save to localStorage
+  localStorage.setItem("blog-list", JSON.stringify(blogPosts));
+
+  // refresh list to include new blog post
+  listBlogPosts();
+}
+
+export { clearDialogInputs, showDialog };
